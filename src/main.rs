5e5900ad89d6d080bdd520a08fe5430bb13f36d5
Dylan Tuttle <jdylantuttle@gmail.com>
2023-06-25 01:01:01 -0400

    Implement REPL, just prints all inputs right back for now

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::env;
+use std::io;
+use std::io::{Write, BufRead};
+
 fn main() {
-    println!("Hello, world!");
+    // Get the command line arguments
+    let args: Vec<String> = env::args().collect();
+
+    // If we haven't passed any arguments in, we can simply start the REPL
+    if args.len() < 2 {
+        println!("jazzy v0.1.0");
+        println!("Enter a line of code and press enter to evaluate it:");
+        
+        let mut exit = false;
+        while !exit {
+            // Print prompt
+            print!("> ");
+            io::stdout().flush().unwrap();
+            
+            // Get user input
+            let line = io::stdin().lock().lines().next().unwrap().unwrap();
+            
+            // Quit the program
+            if line == "quit" || line == "q" || line == "exit" || line == "e" {
+                exit = true;
+            } else {
+                // For now, just repeat the line back
+                println!("{}", line);
+            }
+        }
+    }
 }
