66422809dccb98da1cfa30354d98c006d99c61e0
Dylan Tuttle <jdylantuttle@gmail.com>
2023-07-09 17:57:08 -0400

    Spice up the header that prints when the REPL is entered

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,21 @@ use std::env;
 use std::io;
 use std::io::{BufRead, Write};
 
+use colored::Colorize;
+
 fn main() {
     // Get the command line arguments
     let args: Vec<String> = env::args().collect();
 
     // If we haven't passed any arguments in, we can simply start the REPL
     if args.len() < 2 {
-        println!("jazzy v0.1.0");
-        println!("Enter a line of code and press enter to evaluate it:");
+        // Print the header
+        print_header();
 
         let mut exit = false;
         while !exit {
             // Print prompt
-            print!("> ");
+            print!("=^{}^= ", "..".blue());
             io::stdout().flush().unwrap();
 
             // Get user input
@@ -29,4 +31,31 @@ fn main() {
             }
         }
     }
+}
+
+// This function prints the header that is displayed when the REPL is first run
+fn print_header() {
+    // Get the current version of the compiler
+    let version_number = env!("CARGO_PKG_VERSION");
+
+    // Print the language name and version
+    println!("The jazzy programming language v{}", version_number);
+    println!();
+
+    // Print ASCII art
+    println!("                 /`--'\\");
+    println!("              ==({} w {} )==", "'".blue(), "'".blue());
+    println!("                /       \\");
+    println!("    _           || |  _  \\");
+    println!("   (_)          || |_/    |");
+    println!("    _  __ _ ___((_/_____  /");
+    println!("   | |/ _` |_  /_  / | | |");
+    println!("   | | (_| |/ / / /| |_| |");
+    println!("   | |\\__,_/___/___|\\__| |");
+    println!("  _/ |               __/ |");
+    println!(" |__/               (___/ ");
+    println!();
+
+    // Print the instruction line
+    println!("Write a line of code below and press enter to evaluate it:");
 }
